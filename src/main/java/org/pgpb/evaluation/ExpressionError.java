2e9b2e463be8646690094ea3f0cf603f337b309c
Pablo Padilla <pgpb.padilla@gmail.com>
2018-11-06 21:00:37 +0100

    new: doc: Add TODOs

--- src/main/java/org/pgpb/evaluation/ExpressionError.java
+++ src/main/java/org/pgpb/evaluation/ExpressionError.java
@@ -3,6 +3,7 @@ package org.pgpb.evaluation;
 public enum ExpressionError {
     INVALID_FORMAT ("Invalid format."),
     NEGATIVE_NUMBER ("Only positive numbers are allowed."),
+    // TODO: These two don't really belong here, create SpreadSheetError?
     INVALID_ADDRESS_FORMAT("Invalid cell address format."),
     CELL_NOT_FOUND ("Could not resolve cell address.");
 
